96748872a0ddd9b39b36f31752cdadafc19f1ea8
Kachaykin <kachaykin-m@mail.ru>
2023-04-05 00:13:09 +0500

    Uch

---
+++ Uch/PageMain/PageLogin.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Uch.ApplicationData;
+
+namespace Uch.PageMain
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = AppConnect.model0db.User.FirstOrDefault(x => x.Login == txbLogin.Text && x.Password == psbPassword.Password);
+                if (userObj == null)
+                {
+                    MessageBox.Show("Такого пользователя нет!", "Ошибка при авторизации!",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    switch (userObj.IdRole)
+                    {
+                        case 1:MessageBox.Show("Здравствуйте, Диспетчер" + userObj.Name + "!",
+                            "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            break;
+                        case 2:
+                            MessageBox.Show("Здравствуйте, Механик" + userObj.Name + "!",
+                        "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            break;
+                        default:MessageBox.Show("Данные не обнаружены!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Ошибка" + ex.Message.ToString() + "Критическая работа приложения!",
+                    "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);            
+            }
+        }
+
+
+
+        private void btnRegIn_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.frameMain.Navigate(new PageCreateAcc());
+        }
+    }
+}
+
